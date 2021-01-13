6a3def83cbea710f5d055d252e93f4ed19c2aba5
ThirdRealityStudios <thirdrealitystudios@gmail.com>
2021-01-13 08:05:22 +0100

    Initial release

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Main m = new Main();
+
+        int input = 'A'; // = 65
+
+        int key = '!'; // = 33
+
+        // outcome should be 98 ( 'b' ) then.
+
+        System.out.println((int) m.decryptCaesar((char) input,(char) key));
+    }
+
+    public char crypt(char input, char key) {
+
+        return '\0';
+    }
+
+    public char decrypt(char input, char key) {
+
+        return '\0';
+    }
+
+    public char cryptCaesar(char input, char key) {
+        int match = input + key;
+
+        // Prevent overflow issues
+        char mod = (char) Math.floorMod(match, Character.MAX_VALUE);
+
+        return mod;
+    }
+
+    public char decryptCaesar(char input, char key) {
+        int match = input - key;
+
+        boolean overflow = key > input;
+
+        return overflow ? (char) (Character.MAX_VALUE + match) : (char) match;
+    }
+}
